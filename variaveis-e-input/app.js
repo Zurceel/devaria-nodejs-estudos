481f355a91813506d9a8a2d780372f1aecd4cd86
Zurceel <gabrielcruzsil@gmail.com>
2022-05-16 13:43:25 -0300

    aulas da devaria

---
+++ variaveis-e-input/app.js
@@ -0,0 +1,14 @@
+const readline = require('readLine').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const minhaPrimeriaContateString = 'Minha primera constante';
+console.log(minhaPrimeriaContateString);
+
+let leituraDeCampo;
+
+readline.question('Informe sua idade', input => {
+    leituraDeCampo = input;
+    console.log(`o usuário digitou: ${leituraDeCampo}`);
+})
